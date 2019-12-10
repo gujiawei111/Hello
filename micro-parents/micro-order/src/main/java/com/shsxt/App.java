1a548008dd4478671e311e1e870d5419c74fa076
gujiawei111 <770424953@qq.com>
2019-12-10 21:05:43 +0800

    'dad1aqwewgfdgd'

--- micro-parents/micro-order/src/main/java/com/shsxt/App.java
+++ micro-parents/micro-order/src/main/java/com/shsxt/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-
+        System.out.println("rewrwer");
         System.out.println( "Hello World! orderOewqeqweq!" );
         System.out.println("13123");
 
